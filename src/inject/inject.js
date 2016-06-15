4afade555045800890d907f07a4e46f87fb245c7
John Petty <jptime@users.noreply.github.com>
2016-06-15 01:56:13 -0400

    Add files via upload

---
+++ src/inject/inject.js
@@ -0,0 +1,32 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+
+		
+
+	    var query = /simply/gi
+
+		var replaceHtml = $("body").html().replace(query, '<span style="background-color:yellow"> simply </span>');
+		
+		console.log(query.test(replaceHtml))
+		
+		if (query.test(replaceHtml)){
+
+				var opt = {
+    TemplateType: "basic",
+    title: "Testing!",
+    message: "mayybe this works?",
+    iconUrl: "icons/icon48.png"
+};
+chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
+
+		}
+		
+			
+		$("body").html(replaceHtml)
+
+		}
+	}, 10);
+});
